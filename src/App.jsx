9821cb8733463309cbc6e03b3f83804d89f2a3bb
abraaorochapb <abraaorochapb@gmail.com>
2023-08-03 12:34:57 -0300

    Renderizando listas com componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import RenderingKeys from './components/renderingkeys'
 import ConditionalRender from './components/ConditionalRender'
 import ShowUserName from './components/ShowUserName'
 import CarDetails from './components/CarDetails'
+
+{/* Renderizando listas com componentes */}
+const cars = [
+  {id: 1, brand: 'Ford', km: 100, color: 'vermelho'},
+  {id: 2, brand: 'BMW', km: 200, color: 'azul'},
+  {id: 3, brand: 'Audi', km: 300, color: 'verde'},
+  {id: 4, brand: 'VW', km: 400, color: 'amarelo'},
+  {id: 5, brand: 'Renault', km: 500, color: 'vermelho'},
+]
 function App() {
 
   return (
@@ -12,6 +21,10 @@ function App() {
       <ConditionalRender/>
       <ShowUserName name='Abraao'/>
       <CarDetails brand={'Ford'} km={100} color={'vermelho'}/>
+
+      {cars.map((car) => (
+        <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} />
+      ))}
     </>
   )
 }
